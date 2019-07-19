d3e1045cf3afaae076471429d69dde73cef24ba4
larktavern <tom.stlouis@gmail.com>
2019-07-19 12:31:49 -0400

    interim DNS Route53 coding

--- 01-webotron/webotron/domain.py
+++ 01-webotron/webotron/domain.py
@@ -2,20 +2,22 @@
 
 """Classes for Route 53 domains."""
 
-class Domain Manager:
+class DomainManager:
 	"""Manage a Route 53 domain."""
 	
 	def __init__(self, session):
 		"""Create DomainManager object."""
 		self.session = session
 		self.client = self.session.client('route53')
-
+ 
+# kittenweb.jornaman.net
+# subdomain.kittenweb.jornaman.net
 	def find_hosted_zone(self, domain_name):
 		""" ."""
-		paginator = self.route53_client.get_paginator('list_hosted_zones')
+		paginator = self.client.get_paginator('list_hosted_zones')
 		for page in paginator.paginate():
 			for zone in page['HostedZones']:
-				if domain_name.endswith(zone['Name'][:-1])
+				if domain_name.endswith(zone['Name'][:-1]): #-1 strips off trailing period net.
 					##print(zone)
 					return zone
 
