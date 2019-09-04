a5f9b8637e7ddd086f2ab8cde263c793c205dab0
larktavern <tom.stlouis@gmail.com>
2019-09-04 10:28:28 -0400

    Add example code for Rekognition

---
+++ 03-videolyzer/label-detection.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+bucket = s3.create_bucket(Bucket='tstlvideolyzervideos')
+from pathlib import Path
+pathname = '~\Downloads\Roller Coaster.mp4'
+path = Path(pathname).expanduser().resolve()
+bucket.upload_file(str(path), str(path.name))
+rekognition_client = session.client('rekognition')
+response = rekognition_client.start_label_detection(Video={'S3Object': {'Bucket' : bucket.name, 'Name' : path.name}})
+response
+job_id = response['JobId']
+result = rekognition_client.get_label_detection(JobId=job_id)
+result
+result.keys()
+result['JobStatus']
+result['ResponseMetadata']
+result['VideoMetadata']
+result['Labels']
+result['VideoMetadata']
+result
+result
+result
+result
+result
